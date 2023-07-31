32b2ff8de5ade4a9b7a46c68b585b0d4fb823b07
med20745 <128611207+med20745@users.noreply.github.com>
2023-07-31 09:16:36 +0100

    Update carte.py

--- carte.py
+++ carte.py
@@ -193,7 +193,7 @@ def tracage_moy(x,n):
                 plt.text(b-3.5+0.2, y[a] + 0.2, prob, fontsize=10, color='red')
                 p=str(prob)+ ': 7 points consécutifs sont en augmentation régulière,Régler le processus'
                 legen.append(p)
-             for b in deccroissante :
+            for b in deccroissante :
                 prob+=1
                 for b in a:
                     plt.scatter(b+1, y[b], s=100, color='orange', linewidths=2)
